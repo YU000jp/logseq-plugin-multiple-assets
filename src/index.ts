8e1f7e521cf1b6ef40e21b37a920cb244fc0d45e
YU <111847207+YU000jp@users.noreply.github.com>
2025-06-02 18:54:57 +0900

    feat(): This plugin only supports Logseq ver 0.10.9 and below. #11

--- src/index.ts
+++ src/index.ts
@@ -1,47 +1,71 @@
-import '@logseq/libs'; //https://plugins-doc.logseq.com/
-import { IAsyncStorage } from '@logseq/libs/dist/modules/LSPlugin.Storage';
-import { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user';
-import { setup as l10nSetup, t } from "logseq-l10n"; //https://github.com/sethyuan/logseq-l10n
-import ja from "./translations/ja.json";
+import '@logseq/libs' //https://plugins-doc.logseq.com/
+import { IAsyncStorage } from '@logseq/libs/dist/modules/LSPlugin.Storage'
+import { AppInfo, SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin.user'
+import { setup as l10nSetup, t } from "logseq-l10n" //https://github.com/sethyuan/logseq-l10n
+import ja from "./translations/ja.json"
+let logseqVersion: string = "" //バージョンチェック用
+let logseqVersionMdSupport: boolean = false //バージョンチェック用
 
 
 /* main */
-const main = () => {
+const main = async () => {
 
-  (async () => {
-    try {
-      await l10nSetup({ builtinTranslations: { ja } });
-    } finally {
-      /* user settings */
-      logseq.useSettingsSchema(settingsTemplate());
-      if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300);
-    }
-  })();
+  // バージョンチェック
+
+  logseqVersionMdSupport = await checkLogseqVersion() // MDモデルだった場合はtrue
+  if (logseqVersionMdSupport === false) {
+    // Logseq ver 0.10.9以下にしか対応していない
+    logseq.UI.showMsg("The 'Multiple Files into Assets' plugin only supports Logseq ver 0.10.9 and below.", "warning", { timeout: 5000 })
+    return
+  }
+
+  await l10nSetup({ builtinTranslations: { ja } })
+  /* user settings */
+  logseq.useSettingsSchema(settingsTemplate())
+  if (!logseq.settings) setTimeout(() => logseq.showSettingsUI(), 300)
 
   logseq.Editor.registerBlockContextMenuItem(
     t("💾 Upload multiple assets"),
-    async ({ uuid }) => await embedHelper(uuid, true));
+    async ({ uuid }) => await embedHelper(uuid, true))
 
   logseq.Editor.registerSlashCommand(
     "💾 Upload multiple assets",
-    async ({ uuid }) => await embedHelper(uuid, true));
+    async ({ uuid }) => await embedHelper(uuid, true))
 
   if (logseq.settings!.fromLocalBlockContext) logseq.Editor.registerBlockContextMenuItem(
     t("📂 Insert multiple files from local folder"),
-    async ({ uuid }) => embedHelper(uuid, false));
+    async ({ uuid }) => embedHelper(uuid, false))
 
   logseq.Editor.registerSlashCommand(
     "📂 Insert multiple files from local folder",
-    async ({ uuid }) => embedHelper(uuid, false));
+    async ({ uuid }) => embedHelper(uuid, false))
 
-};/* end_main */
+}/* end_main */
 
 
+// MDモデルかどうかのチェック DBモデルはfalse
+const checkLogseqVersion = async (): Promise<boolean> => {
+  const logseqInfo = (await logseq.App.getInfo("version")) as AppInfo | any
+  //  0.11.0もしくは0.11.0-alpha+nightly.20250427のような形式なので、先頭の3つの数値(1桁、2桁、2桁)を正規表現で取得する
+  const version = logseqInfo.match(/(\d+)\.(\d+)\.(\d+)/)
+  if (version) {
+    logseqVersion = version[0] //バージョンを取得
+    // console.log("logseq version: ", logseqVersion)
+
+    // もし バージョンが0.10.9以下ならば、logseqVersionMdSupportをtrueにする
+    if (logseqVersion.match(/0\.(([0-9])|10\.([0-9]|0))\d*/)) {
+      logseqVersionMdSupport = true
+      // console.log("logseq version is 0.10.9 or lower")
+      return true
+    } else logseqVersionMdSupport = false
+  } else logseqVersion = "0.0.0"
+  return false
+}
 
 interface Files {
-  name: string;
-  type: string;
-  path?: string;
+  name: string
+  type: string
+  path?: string
 }
 
 
@@ -49,98 +73,98 @@ async function embedHelper(
   uuid: string,
   isAsset: boolean
 ): Promise<void> {
-  const fileInput: HTMLInputElement = document.createElement("input");
-  const btn: HTMLButtonElement = document.createElement("button");
+  const fileInput: HTMLInputElement = document.createElement("input")
+  const btn: HTMLButtonElement = document.createElement("button")
 
-  fileInput.type = "file";
-  fileInput.multiple = true;
-  const storage = logseq.Assets.makeSandboxStorage() as IAsyncStorage;
+  fileInput.type = "file"
+  fileInput.multiple = true
+  const storage = logseq.Assets.makeSandboxStorage() as IAsyncStorage
   fileInput.onchange = async (): Promise<void> => {
-    fileInput.remove();
-    btn.remove();
+    fileInput.remove()
+    btn.remove()
     try {
-      const files = fileInput.files as FileList | null;
-      if (files === null) return;
+      const files = fileInput.files as FileList | null
+      if (files === null) return
       for (const file of Array.from(files)) {
-        const { type, path, name } = file as Files;
+        const { type, path, name } = file as Files
         if (!type || !path || !name) {
-          logseq.UI.showMsg(`Error writing file`, 'error');
-          continue;
+          logseq.UI.showMsg(`Error writing file`, 'error')
+          continue
         }
 
-        let rename: string;
-        let duplicate: Boolean = false;
+        let rename: string
+        let duplicate: Boolean = false
         if (logseq.settings!.timestamp === true) {
           //「yyyymmdd」のフォーマットにしたい
-          rename = `${timestamp()}_` + name;
+          rename = `${timestamp()}_` + name
         } else {
           ////ファイル重複チェック
           if (await storage.hasItem(name) as boolean) {
             if (logseq.settings!.overwrite === "timestamp") {
               //「yyyymmdd」のフォーマットにしたい
-              rename = `${timestamp()}_` + name;
+              rename = `${timestamp()}_` + name
             } else if (logseq.settings!.overwrite === "overwrite") {
-              await storage.removeItem(name);
-              rename = name;
+              await storage.removeItem(name)
+              rename = name
             } else {
               //skip
-              rename = name;
-              logseq.UI.showMsg(`File skipped: ${name}`, 'warning'), { timeout: 1300 };
-              duplicate = true;
+              rename = name
+              logseq.UI.showMsg(`File skipped: ${name}`, 'warning'), { timeout: 1300 }
+              duplicate = true
             }
           } else {
-            rename = name;
+            rename = name
           }
         }
         if (isAsset && duplicate === false) {
           try {
-            const fileReader = new FileReader();
-            fileReader.onload = () => storage.setItem(rename, fileReader.result as string);
-            fileReader.readAsArrayBuffer(file);
+            const fileReader = new FileReader()
+            fileReader.onload = () => storage.setItem(rename, fileReader.result as string)
+            fileReader.readAsArrayBuffer(file)
           } catch (error) {
-            logseq.UI.showMsg(`Error writing file: ${rename}`, 'error');
-            continue;
+            logseq.UI.showMsg(`Error writing file: ${rename}`, 'error')
+            continue
           } finally {
-            logseq.UI.showMsg(t("File saved to assets"), 'success', { timeout: 1300 });
+            logseq.UI.showMsg(t("File saved to assets"), 'success', { timeout: 1300 })
           }
         }
-        let isEmbed: boolean;
-        let icon: string;
+        let isEmbed: boolean
+        let icon: string
         if (type.startsWith("image")) {
-          icon = "🖼";
-          isEmbed = true;
+          icon = "🖼"
+          isEmbed = true
         } else if (type.startsWith("video")) {
-          icon = "📹";
-          isEmbed = true;
+          icon = "📹"
+          isEmbed = true
         } else if (type.startsWith("audio")) {
-          icon = "🎧";
-          isEmbed = true;
+          icon = "🎧"
+          isEmbed = true
         } else if (type.startsWith("text/html")) {
-          icon = "📄";
-          isEmbed = false;
+          icon = "📄"
+          isEmbed = false
         } else if (type.startsWith("text/plain")) {
-          icon = "📄";
-          isEmbed = false;
+          icon = "📄"
+          isEmbed = false
         } else if (type.startsWith("application/pdf")) {
-          icon = "📰";
-          isEmbed = true;
+          icon = "📰"
+          isEmbed = true
         } else {
-          icon = "📑";
-          isEmbed = false;
+          icon = "📑"
+          isEmbed = false
         }
-        setTimeout(() => logseq.Editor.insertBlock(uuid, returnFilePath(isAsset, icon, rename, name, path, isEmbed), { focus: true }), 30);
+        setTimeout(() => logseq.Editor.insertBlock(uuid, returnFilePath(isAsset, icon, rename, name, path, isEmbed), { focus: true }), 30)
       }
     } finally {
-      setTimeout(() => logseq.Editor.selectBlock(uuid), 300);
+      setTimeout(() => logseq.Editor.selectBlock(uuid), 300)
     }
-  };
+  }
 
-  btn.addEventListener("click", () => fileInput.click());
-  btn.click();
+  btn.addEventListener("click", () => fileInput.click())
+  btn.click()
 }
 
 
-const timestamp = () => new Date().toISOString().slice(0, 19).replace(/[-:]/g, "").replace("T", "_");
+const timestamp = () => new Date().toISOString().slice(0, 19).replace(/[-:]/g, "").replace("T", "_")
 
 
 function returnFilePath(
@@ -159,7 +183,7 @@ function returnFilePath(
       : isEmbed ?
         `![${emoji} ${name}](file://${path})`
         : `[${emoji} ${name}](file://${path})`
-  );
+  )
 }
 
 
@@ -188,7 +212,7 @@ const settingsTemplate = (): SettingSchemaDesc[] => [
     description: t("default: `true` (⚠️need to turn off this plugin or restart Logseq to take effect)"),
     default: true,
   }
-];
+]
 
 
-logseq.ready(main).catch(console.error);
+logseq.ready(main).catch(console.error)
